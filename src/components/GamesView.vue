2a42943839748308d8aab507dbd325ef5ef4fd78
Mario Blazquez <mblazquezdepaz@gmail.com>
2024-07-21 11:22:33 +0200

    first commit

---
+++ src/components/GamesView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="jumbotron vertical-center">
+    <div class="container">
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/sketchy/bootstrap.min.css"
+        integrity="sha384-RxqHG2ilm4r6aFRpGmBbGTjsqwfqHOKy1ArsMhHusnRO47jcGqpIQqlQK/kmGy9R"
+        crossorigin="anonymous"
+      />
+
+      <p>Games Library</p>
+    </div>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
 import SharkView from "../components/SharkView.vue";
+import HomeView from "../components/HomeView.vue";
+import GamesView from "../components/GamesView.vue";
 
 const routes = [
   {
     path: "/shark",
     name: "shark",
     component: SharkView,
+  },
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/games",
+    name: "games",
+    component: GamesView,
   },
 ];
 
