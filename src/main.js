9f939dd1b0400b60a742d8e1710aadb4f39e22f1
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2024-07-21 13:33:05 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
-import { createApp } from "vue";
+import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "bootstrap/dist/css/bootstrap.css";
+// install bootstrap first
+import 'bootstrap/dist/css/bootstrap.css';
+// Importing the bootstrapvue library
+import BootstrapVue from 'bootstrap-vue';
+
+
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue)
+
+
+
+
 
 createApp(App).use(router).mount("#app");
